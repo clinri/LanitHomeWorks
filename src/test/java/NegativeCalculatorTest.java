b545a799811df6aa9bb2d72924e91b4ff3182ed0
Andrei <clinri@ya.ru>
2022-02-25 11:43:12 +0300

    homework5. version 3

--- src/test/java/NegativeCalculatorTest.java
+++ src/test/java/NegativeCalculatorTest.java
@@ -1,19 +1,14 @@
 import model.Calculator;
 import model.CalculatorException;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class NegativeCalculatorTest {
-    public static final Logger LOG = LogManager.getLogger(NegativeCalculatorTest.class);
-
     @DataProvider
     public Object[][] negativeData() {
         String max = String.valueOf(Integer.MAX_VALUE);
         String min = String.valueOf(Integer.MIN_VALUE);
         System.out.println(max);
-        LOG.info("DataProvider запущен");
         return new Object[][]{
                 {"/", "3", "0"},
                 {"+", max, "1"},
--- src/test/java/PositiveCalculatorTest.java
+++ src/test/java/PositiveCalculatorTest.java
@@ -1,12 +1,9 @@
 import model.Calculator;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class PositiveCalculatorTest {
-    public static final Logger LOG = LogManager.getLogger(PositiveCalculatorTest.class);
 
     @DataProvider
     public Object[][] positiveData() {
@@ -14,7 +11,6 @@ public class PositiveCalculatorTest {
         String maxMunusOne = String.valueOf(Integer.MAX_VALUE - 1);
         String min = String.valueOf(Integer.MIN_VALUE);
         String minPlusOne = String.valueOf(Integer.MIN_VALUE + 1);
-        LOG.info("DataProvider запущен");
         return new Object[][]{
                 {"+", "2", "2", "4"},
                 {"+", maxMunusOne, "1", max},
@@ -27,7 +23,6 @@ public class PositiveCalculatorTest {
 
     @Test(dataProvider = "positiveData")
     public static void positiveTest(String operator, String a, String b, String result) {
-        LOG.info("Тест запущен");
         String[] params = {operator, a, b};
         Calculator calculator = new Calculator();
         Assert.assertEquals(calculator.execute(params), result, "Несовпадение с ожидаемым значением");
