9a7ab800644dd0e0a92b636d256e61ac2e0c6c20
Andrei <clinri@ya.ru>
2022-02-25 14:49:17 +0300

    homework5. version 7

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,20 @@
 import model.Calculator;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String line = in.nextLine();
-        System.out.println(Calculator.execute(line.split(" ")));
+        String[] paramsIn = line.split(" ");
+        System.out.println(paramsIn.length);
+        String[] params = Arrays.copyOf(paramsIn, 3);
+        for (int i = 0; i < 3; i++) {
+            if (params[i] == null) {
+                params[i] = "null";
+            }
+        }
+        System.out.println(Calculator.execute(params));
     }
 }
