4443a7c3f7f3d30c9fc8642116b5efb07f94a621
Andrei <clinri@ya.ru>
2022-02-25 15:00:56 +0300

    homework5. version 8

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,15 +6,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String line = in.nextLine();
-        String[] paramsIn = line.split(" ");
-        System.out.println(paramsIn.length);
-        String[] params = Arrays.copyOf(paramsIn, 3);
-        for (int i = 0; i < 3; i++) {
-            if (params[i] == null) {
-                params[i] = "null";
-            }
-        }
-        System.out.println(Calculator.execute(params));
+        String[] paramsIn = new String[3];
+        paramsIn[0] = in.nextLine();
+        paramsIn[1] = in.nextLine();
+        paramsIn[2] = in.nextLine();
+        System.out.println(Calculator.execute(paramsIn));
     }
 }
