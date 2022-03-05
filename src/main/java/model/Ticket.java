f2ee8345ddb57bbc2a42c33e4893aa4674c7619f
Andrei Borisov <clinri@ya.ru>
2022-03-05 18:22:18 +0300

    home work 6. test 5

--- src/main/java/model/Ticket.java
+++ src/main/java/model/Ticket.java
@@ -1,8 +1,6 @@
 package model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.google.gson.annotations.SerializedName;
-
 import java.util.Date;
 import java.util.Objects;
 
@@ -11,21 +9,21 @@ import java.util.Objects;
  */
 public class Ticket {
     // serialized поля, геттеры и сеттеры
-    private int id; //readOnly: true
+    private Integer id; //readOnly: true
     private String due_date;
     @JsonIgnore
     private String assigned_to; //pattern: ^[\w.@+-]+$
     private String title; //maxLength: 200
-    private Date created; //Date this ticket was first created
-    private Date modified; //Date this ticket was most recently changed.
+    private String created; //Date this ticket was first created
+    private String modified; //Date this ticket was most recently changed.
     private String submitter_email; //nullable. The submitter will receive an email for all public follow-ups left for this task.
     private Enum<Status> status;
     private boolean on_hold; //If a ticket is on hold, it will not automatically be escalated.
     private String description; //nullable. The content of the customers query.
     private String resolution; //nullable. The resolution provided to the customer by our staff.
-    private Enum<Priority> priority; //1 = Highest Priority, 5 = Low Priority
+    private Integer priority; //1 = Highest Priority, 5 = Low Priority
     @JsonIgnore
-    private Date lastEscalation; // readOnly: true. Updated automatically by management/commands/escalate_tickets.py
+    private String lastEscalation; // readOnly: true. Updated automatically by management/commands/escalate_tickets.py
     private String secret_key; //Secret key needed for viewing/editing ticket by non-logged in users
     private int queue;
     @JsonIgnore
@@ -33,12 +31,25 @@ public class Ticket {
     @JsonIgnore
     private int merged_to; //nullable
 
+    public Ticket(String due_date, String title, String created, String modified, String submitter_email,
+                  Enum<Status> status, boolean on_hold, Integer priority, int queue) {
+        this.due_date = due_date;
+        this.title = title;
+        this.created = created;
+        this.modified = modified;
+        this.submitter_email = submitter_email;
+        this.status = status;
+        this.on_hold = on_hold;
+        this.priority = priority;
+        this.queue = queue;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Ticket ticket = (Ticket) o;
-        return id == ticket.id;
+        return id.equals(ticket.id);
     }
 
     @Override
--- src/test/java/api/BaseTest.java
+++ src/test/java/api/BaseTest.java
@@ -3,13 +3,26 @@ package api;
 import io.restassured.RestAssured;
 import model.*;
 import org.testng.annotations.BeforeClass;
+import sun.security.jca.GetInstance;
+
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Абстрактный класс, содержащий общие для всех тестов методы
  */
 public abstract class BaseTest {
     protected static String token;
+
     @BeforeClass
     public void prepare() {
         try {
@@ -26,18 +39,43 @@ public abstract class BaseTest {
         if (port == 0) {
             throw new RuntimeException("В файле \"config.properties\" отсутствует значение \"port.uri\"");
         }
-        RestAssured.requestSpecification = Specifications.requestSpec(baseUri,port);
+        RestAssured.requestSpecification = Specifications.requestSpec(baseUri, port);
         // подготовлены глобальные преднастройки для запросов
     }
 
     protected Ticket buildNewTicket(Status status, int priority) {
         // todo: создать объект с тестовыми данными
+//        Ticket ticket = new Ticket(new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime()),
+//                "Test title",
+//                new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime()),
+//
+//
+//                )
         return null;
     }
 
     protected Ticket createTicket(Ticket ticket) {
         // todo: отправить HTTP запрос для создания тикета
         return null;
+    }
+
+    public static void main(String[] args) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS");
+        LocalDateTime when =
+                LocalDateTime.of(2016, Month.AUGUST, 12, 9, 38, 12, 123456789);
+        LocalDateTime time = LocalDateTime.parse("2022-03-05T06:16:49.018615");
+        String DATE_TIME_FORMAT_PATTERN = "yyyy-MM-dd'T'HH:mm:ss";
+        DateTimeFormatter formatter1 = new DateTimeFormatterBuilder()
+                .appendPattern(DATE_TIME_FORMAT_PATTERN)
+                .appendFraction(ChronoField.MICRO_OF_SECOND, 6, 6, true)
+                .toFormatter();
+        System.out.println(LocalDateTime.now().format(formatter1));
+        System.out.println(when.format(formatter1));
+        System.out.println(when.format(dtf));
+        System.out.println(time.format(dtf));
+
+        System.out.println(Instant.now().plus(1, ChronoUnit.DAYS));
+
     }
 
 }
